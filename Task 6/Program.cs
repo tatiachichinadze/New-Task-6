dcb1e828d4b0f9d959a78a95e8b2ecfa7b16ec1a
Tatia Chichinadze <echichinadze@credo.ge>
2024-11-01 14:09:21 +0400

    Add project files.

---
+++ Task 6/Program.cs
@@ -0,0 +1,32 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter array size: ");
+        int size = int.Parse(Console.ReadLine());
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine($"Enter an integer {i + 1}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Enter index (0 to " + (size - 1) + "): ");
+        int index = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Result: " + SumNumber(array[index]));
+    }
+
+    static int SumNumber(int num)
+    {
+        int sum = 0;
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
